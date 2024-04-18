e203597321374d4f45e3f6ec65257cfe8a8f45a3
Prince <princejaj67@gmail.com>
2024-04-18 16:33:51 +0530

    Adding components and layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React,{ReactNode} from 'react'
+
+const RootLayout = ({children} : {children : ReactNode}) => {
+  return (
+    <div>
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default RootLayout
+
