b4adba845dc07dd72faaccc10eccbd858db1b0af
Prince <princejaj67@gmail.com>
2024-04-18 17:19:31 +0530

    Implemented SideBar

---
+++ components/SideBar.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import { sideBarLinks } from '@/constants';
+import { usePathname } from 'next/navigation'
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+const SideBar = () => {
+    const pathname = usePathname();
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+        <div className="flex flex-1 flex-col gap-6">
+            {sideBarLinks.map((link)=>{
+                const isActive = pathname === link.route || pathname.startsWith(link.route);
+                return(<Link key={link.label} href={link.route} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{'bg-blue-1': isActive})}>
+                  <Image src={link.imgUrl} alt={link.label} className='' height={24} width={24}/> {link.label}</Link>)
+            })}
+        </div>
+    </section>
+  )
+}
+
+export default SideBar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config = {
       colors: {
         dark : {
           1 : '#1C1F2E',
-          2 : '#1C1F2E'
+          2 : '#161925'
         },
         blue : {
           1 : '#0E78F9'
