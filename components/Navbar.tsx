08f6398ce49cbfb6758ce80bcd6056a8243e708c
Prince <princejaj67@gmail.com>
2024-04-19 21:04:20 +0530

    Added authentication and improving signin and signup pages in clerk Authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { SignInButton, SignedIn,SignedOut,UserButton } from '@clerk/nextjs'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex-between fixed z-[10] w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link className="flex items-center gap-1" href="/">
+        <Image
+          src="/icons/logo.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Comfront
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        
 
-export default Navbar
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -11,8 +11,8 @@ const SideBar = () => {
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
         <div className="flex flex-1 flex-col gap-6">
             {sideBarLinks.map((link)=>{
-                const isActive = pathname === link.route || pathname.startsWith(link.route);
-                return(<Link key={link.label} href={link.route} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{'bg-blue-1': isActive})}>
+                const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
+                return(<Link key={link.label} href={link.route} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{'bg-gradient-to-r from-blue-600 to-violet-600': isActive})}>
                   <Image src={link.imgUrl} alt={link.label} className='' height={24} width={24}/> {link.label}</Link>)
             })}
         </div>
