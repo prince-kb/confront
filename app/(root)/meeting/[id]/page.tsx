338c62946cf20c0910fa58a1d42b700de2f73360
Prince <princejaj67@gmail.com>
2024-04-22 13:21:45 +0530

    Added video layout and calling

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+"use client";
+import { useGetCallById } from '@/Hooks/useGetCallById';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { Loader } from 'lucide-react';
+import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: { id: string }}) => {
+
+const Meeting = ({ params : {id} }: { params: { id: string }}) => {
+  const {call,isCallLoading} = useGetCallById(id)
+  const {user,isLoaded} = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false)
+
+  if(!isLoaded || isCallLoading) return <Loader/>
   return (
-    <div>Meeting room : #{params.id}</div>
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+        {!isSetupComplete ? (
+          <MeetingSetup setIsSetupComplete={setIsSetupComplete}/>
+        ): (
+          <MeetingRoom/>
+        )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
